cca326c77e9035e6c4e9374e16110ccd58e33e24
Paul Savala <paulsavala@gmail.com>
2020-11-02 14:47:33 -0600

    Implemented attention

--- Code/models/seq2seq.py
+++ Code/models/seq2seq.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Embedding, LSTM, Dense
 
@@ -9,17 +10,20 @@ from utils.integers import undo_one_hot_matrix
 
 import numpy as np
 import re
+import time
+import os
 
 
 class GenericSeq2Seq(GenericModel):
-    def __init__(self, encoder_units=None, batch_size=None, vocab_size=None, input_seq_length=None, name=None,
-                 target_seq_length=None, decoder_units=None, loss='categorical_crossentropy', optimizer='adam',
+    def __init__(self, encoder_units=None, batch_size=None, vocab_size=None, embedding_dim=None, input_seq_length=None,
+                 name=None, target_seq_length=None, decoder_units=None, loss='categorical_crossentropy', optimizer='adam',
                  metrics=['accuracy'], int_encoder=None):
         super().__init__(name)
         self.encoder_units = encoder_units
         self.decoder_units = decoder_units
         self.batch_size = batch_size
         self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
         self.input_seq_length = input_seq_length
         self.target_seq_length = target_seq_length
         self.loss = loss
@@ -166,3 +170,175 @@ class Seq2Seq(GenericSeq2Seq):
                 incorrect.append(f'{X_true_cleaned}, {pred_val}, {y_true_val}')
         list_to_csv(correct, 'correct_preds.csv', headers='X_true, y_true')
         list_to_csv(incorrect, 'incorrect_preds.csv', headers='X_true, y_pred, y_true')
+
+
+class Encoder(GenericSeq2Seq):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(self.vocab_size, self.embedding_dim)
+        self.lstm = LSTM(units=self.encoder_units, return_state=True, name='Encoder_LSTM')
+
+    def call(self, x, hidden):
+        x = self.embedding(x)
+        output, state = self.lstm(x, initial_state=hidden)
+        return output, state
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_size, self.encoder_units))
+
+
+class BahdanauAttention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.attention_units = units
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, query, values):
+        # query hidden state shape == (batch_size, hidden size)
+        # query_with_time_axis shape == (batch_size, 1, hidden size)
+        # values shape == (batch_size, max_len, hidden size)
+        # we are doing this to broadcast addition along the time axis to calculate the score
+        query_with_time_axis = tf.expand_dims(query, 1)
+
+        # score shape == (batch_size, max_length, 1)
+        # we get 1 at the last axis because we are applying score to self.V
+        # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+        score = self.V(tf.nn.tanh(
+            self.W1(query_with_time_axis) + self.W2(values)))
+
+        # attention_weights shape == (batch_size, max_length, 1)
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+class Decoder(GenericSeq2Seq):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(self.vocab_size, self.embedding_dim)
+        self.lstm = LSTM(self.decoder_units,
+                         return_sequences=True,
+                         return_state=True,
+                         recurrent_initializer='glorot_uniform')
+        self.fc = tf.keras.layers.Dense(self.vocab_size)
+
+        # used for attention
+        self.attention = BahdanauAttention(self.decoder_units)
+
+    def call(self, x, hidden, enc_output):
+        # enc_output shape == (batch_size, max_length, hidden_size)
+        context_vector, attention_weights = self.attention(hidden, enc_output)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.lstm(x)
+
+        # output shape == (batch_size * 1, hidden_size)
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        # output shape == (batch_size, vocab)
+        x = self.fc(output)
+
+        return x, state, attention_weights
+
+
+class Seq2SeqWithAttention(GenericSeq2Seq):
+    def __init__(self):
+        super(Seq2SeqWithAttention, self).__init__()
+
+        self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
+
+        self.encoder = None
+        self.decoder = None
+
+    def loss_function(self, real, pred):
+        # Mask zero padding
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = self.loss_object(real, pred)
+
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
+
+        return tf.reduce_mean(loss_)
+
+    def build_model(self):
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def train(self, X, y, epochs=1, early_stopping=False, validation_split=0.0,
+              optimizer=None, optimizer_params=dict(), loss=None, metrics=None):
+
+        checkpoint_dir = './training_checkpoints'
+        checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+        checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+                                         encoder=self.encoder,
+                                         decoder=self.decoder)
+
+        dataset = tf.data.Dataset.from_tensor_slices((X, y)).shuffle(len(X))
+        dataset = dataset.batch(self.batch_size, drop_remainder=True)
+
+        steps_per_epoch = len(X) // self.batch_size
+
+        for epoch in range(epochs):
+            start = time.time()
+
+            enc_hidden = self.encoder.initialize_hidden_state()
+            total_loss = 0
+
+            for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
+                batch_loss = self.train_step(inp, targ, enc_hidden, optimizer)
+                total_loss += batch_loss
+
+                if batch % 100 == 0:
+                    print('Epoch {} Batch {} Loss {:.4f}'.format(epoch + 1,
+                                                                 batch,
+                                                                 batch_loss.numpy()))
+            # saving (checkpoint) the model every 2 epochs
+            if (epoch + 1) % 2 == 0:
+                checkpoint.save(file_prefix=checkpoint_prefix)
+
+            print('Epoch {} Loss {:.4f}'.format(epoch + 1,
+                                                total_loss / steps_per_epoch))
+            print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+    @tf.function
+    def train_step(self, inp, targ, enc_hidden, optimizer):
+        loss = 0
+
+        with tf.GradientTape() as tape:
+            enc_output, enc_hidden = self.encoder(inp, enc_hidden)
+
+            dec_hidden = enc_hidden
+
+            dec_input = tf.expand_dims([targ_lang.word_index['\t']] * self.batch_size, 1)
+
+            # Teacher forcing - feeding the target as the next input
+            for t in range(1, targ.shape[1]):
+                # passing enc_output to the decoder
+                predictions, dec_hidden, _ = self.decoder(dec_input, dec_hidden, enc_output)
+
+                loss += self.loss_function(targ[:, t], predictions)
+
+                # using teacher forcing
+                dec_input = tf.expand_dims(targ[:, t], 1)
+
+        batch_loss = (loss / int(targ.shape[1]))
+
+        variables = self.encoder.trainable_variables + self.decoder.trainable_variables
+
+        gradients = tape.gradient(loss, variables)
+
+        optimizer.apply_gradients(zip(gradients, variables))
+
+        return batch_loss
